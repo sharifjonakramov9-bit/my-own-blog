39ec7df083bda19eb0e2d6f115679bb163648765
sharifjonakramov9.bit <sharifjonakramov9@gemail.com>
2026-01-17 08:25:18 +0300

    upgrade

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from .views import home, blog_list, blog_detail, blog_create
 
 urlpatterns = [
     path('', home, name='home'), 
-    path('blog', blog_list, name='blog_list'), 
-    path('blog/<int:id>/', blog_detail, name='blog_detail'), 
+    path('blog/', blog_list, name='blog_list'), 
+    path('blog/<int:pk>/', blog_detail, name='blog_detail'), 
     path('blog/create', blog_create, name='blog_create'),    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,33 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
+
+from blog.forms import BlogForm
 from .models import Blog
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'blog/home.html')
 
 
 def blog_list(request):
-    blogs = Blog.objects.all()
+    query = request.GET.get('q')
+    blogs = Blog.objects.all().order_by('-created_at')
 
-    search = request.GET.get('search')
-    category = request.GET.get('category')
-
-    if search:
-        blogs = blogs.filter(title__icontain=search)
-
-    if category:
-        blogs = blogs.filter(category=category)
-
-    return render(request, 'blog_list.html', {'blogs': blogs})
+    if query:
+        blogs = blogs.filter(title_icontains=query)
 
 
-def blog_detail(request, id):
-    blog = Blog.objects.get(id=id)
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
     return render(request, 'blog_list.html', {'blogs': blog})
 
 
 def blog_create(request):
     if request.method == 'POST':
-        Blog.objects.create(
-            title=request.POST['title'],
-            content=request.POST['content'],
-            category=request.POST['category'],
-        )
-        return redirect('blog_list')
-    
-    return render(request, 'blog_create.html')
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+        else:
+            form = BlogForm()
+        
+        return render(request, 'blog/blog_create.html', {'form': form})
